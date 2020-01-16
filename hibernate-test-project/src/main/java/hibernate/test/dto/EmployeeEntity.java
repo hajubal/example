23abbcc2c33ebbecf76d440196150c1a4fda3cd8
hajubal <hajubal@DESKTOP-S1CMFDH>
2020-01-16 23:56:45 +0900

    hibernate select, insert, delete testcase 추가

--- hibernate-test-project/src/main/java/hibernate/test/dto/EmployeeEntity.java
+++ hibernate-test-project/src/main/java/hibernate/test/dto/EmployeeEntity.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -20,6 +21,7 @@ public class EmployeeEntity implements Serializable {
 	
 	private static final long serialVersionUID = -1798070786993154676L;
 	
+	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO) 
 	@Column(name = "ID", unique = true, nullable = false)
 	private long id;
--- hibernate-test-project/src/test/java/hibernate/test/AppTest.java
+++ hibernate-test-project/src/test/java/hibernate/test/AppTest.java
@@ -1,38 +1,49 @@
 package hibernate.test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import java.util.List;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.junit.Test;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+import hibernate.test.dto.EmployeeEntity;
+
+public class AppTest {
+	@Test
+	public void 저장_조회_삭제() {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+       
+		//Add new Employee object
+		EmployeeEntity emp = new EmployeeEntity();
+		emp.setEmail("demo-user@mail.com");
+		emp.setFirstName("demo");
+		emp.setLastName("user");
+		
+		Long id = (Long) session.save(emp);
+		
+		Query query = session.createQuery("from EmployeeEntity where id = :id");
+		query.setParameter("id", id);
+		
+		List<EmployeeEntity> resultList = (List<EmployeeEntity>)query.list();
+		
+		assertNotNull(resultList);
+		
+		assertEquals(1, resultList.size());
+		
+		EmployeeEntity newEntity = resultList.iterator().next();
+		
+		assertEquals(emp.getEmail(), newEntity.getEmail());
+		
+		session.delete(newEntity);
+		
+		newEntity = (EmployeeEntity) session.get(EmployeeEntity.class, id);
+
+		assertNull(newEntity);
+		
+		session.getTransaction().commit();
+	}
+	
 }
